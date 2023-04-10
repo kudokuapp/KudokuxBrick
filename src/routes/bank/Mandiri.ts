96ea6320e535d0a232c94f10435a0d6bafa11f5d
Furqon Wilogo <fdwilogo@gmail.com>
2023-04-10 13:08:35 +0700

    make new route transactionto

--- src/routes/bank/Mandiri.ts
+++ src/routes/bank/Mandiri.ts
@@ -219,6 +219,75 @@ router.post('/transaction', async (req, res) => {
   }
 });
 
+router.post('/transactionto', async (req, res) => {
+  try {
+    const { type, accessToken, from, to } = req.body;
+
+    // start verification
+    const authHeader = req.headers.authorization;
+
+    let brickUserId: string | null = null;
+
+    if (!authHeader) return res.status(400).send('Invalid header');
+
+    if (type === 'kudoku-app') {
+      const { userId: _userId } = decodeAuthHeaderApp(authHeader, res);
+
+      const collection = req.db.collection('User');
+
+      const userId = new ObjectId(_userId);
+
+      const user = await collection.findOne({ _id: userId });
+
+      if (!user)
+        return res.status(404).send(`User with ID ${_userId} not found`);
+
+      brickUserId = _userId;
+    } else if (type === 'BGST') {
+      const { email } = decodeAuthHeaderBgst(authHeader, res);
+
+      const result = await req.pg.query('SELECT * FROM User WHERE email = $1', [
+        email,
+      ]);
+
+      if (result.rows.length === 0)
+        return res.status(404).send(`User with email ${email} not found`);
+
+      brickUserId = email;
+    } else {
+      return res.status(400).send('Invalid type');
+    }
+
+    if (brickUserId === null)
+      return res.status(404).send(`brickUserId is null`);
+    // end verification
+
+    const transactionUrl = brickUrl(`/v1/transaction/list`);
+
+    const transactionOptions = {
+      method: 'GET',
+      url: transactionUrl.href,
+      params: { from, to },
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    };
+
+    const {
+      data: { data: transactionData },
+    }: { data: { data: BrickTransactionData[] } } = await axios.request(
+      transactionOptions
+    );
+
+    res.status(200).json(transactionData);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('Fatal error');
+  }
+});
+
 router.post('/refresh', async (req, res) => {
   try {
     const { type, accessToken, from } = req.body;
